ryan-0324/vendetta-plugins

commit a4597f1457dfa0430ae6c9037d1841bc75aeccd5

Fixed `YouScreen` and `BottomTabBar` not updating the theme and effect used when plugin is loaded/unloaded + small refactor of `patchUseProfileTheme`

plugins/FakeProfileThemesAndEffects/src/index.ts
@@ -1,3 +1,5 @@
+import { FluxDispatcher } from "@lib/flux";
+import { UserProfileStore, UserStore } from "@lib/stores";
 import {
     patchGetPurchase,
     patchGetUserProfile,
@@ -7,6 +9,18 @@ import {
     patchUserProfileEditForm
 } from "@patches";
 import { Settings } from "@ui/pages";
+
+/** Updates the profile theme and effect used by YouScreen and BottomTabBar. */
+function updateProfileThemeAndEffect() {
+    const user = UserStore.getCurrentUser();
+    const user_profile = UserProfileStore.getUserProfile(user.id)!;
+    FluxDispatcher.dispatch({
+        type: "USER_PROFILE_FETCH_SUCCESS",
+        user,
+        user_profile,
+        connected_accounts: user_profile.connectedAccounts
+    });
+}
 
 const patches: (() => boolean)[] = [];
 
@@ -20,11 +34,13 @@ export default {
             patchUseProfileTheme(),
             patchUserProfileEditForm()
         );
+        updateProfileThemeAndEffect();
     },
     onUnload() {
         patches.forEach(unpatch => {
             unpatch();
         });
+        updateProfileThemeAndEffect();
     },
     settings: Settings
 };

plugins/FakeProfileThemesAndEffects/src/lib/flux.ts
@@ -1,9 +1,7 @@
 import { FluxDispatcher as _FluxDispatcher } from "@vendetta/metro/common";
 
 export const FluxDispatcher: {
-    dispatch: (payload: {
-        type?: string | undefined;
-    }) => Promise<any>;
-    subscribe: (type?: string | undefined, onDispatch?: (payload: any) => void) => void;
-    unsubscribe: (type?: string | undefined, onDispatch?: (payload: any) => void) => void;
+    dispatch: (payload: { type?: string | undefined; } & Record<PropertyKey, any>) => Promise<any>;
+    subscribe: (type?: string | undefined, onDispatch?: ((payload: any) => void) | undefined) => void;
+    unsubscribe: (type?: string | undefined, onDispatch?: ((payload: any) => void) | undefined) => void;
 } = _FluxDispatcher as any;

plugins/FakeProfileThemesAndEffects/src/patches/patchUseProfileTheme.ts
@@ -122,43 +122,44 @@ type useProfileThemeColorsArgs = [
 
 type useProfileThemeColorsRet = [number | null, number | null];
 
-let fallback = false;
-
-const funcParent = findByName("useProfileTheme", false)
-    ?? (fallback = true, findByName("useProfileThemeColors", false))
-    ?? { default: () => undefined };
-
-export const patchUseProfileTheme = () => after("default", funcParent, (!fallback
-    ? ([props]: useProfileThemeArgs, profileTheme: useProfileThemeRet) => {
-        const { user } = props;
-        if (
-            (user != null && user.id === previewUserId
-            || "pendingThemeColors" in props)
-            && showPreview
-        ) {
-            if (primaryColor !== null) {
-                profileTheme.theme = getProfileTheme(primaryColor);
-                profileTheme.primaryColor = primaryColor;
-                profileTheme.secondaryColor = accentColor ?? primaryColor;
-            } else if (accentColor !== null) {
-                profileTheme.theme = getProfileTheme(accentColor);
-                profileTheme.primaryColor = accentColor;
-                profileTheme.secondaryColor = accentColor;
+export const patchUseProfileTheme = (() => {
+    let funcParent = findByName("useProfileTheme", false);
+    if (funcParent)
+        return () => after("default", funcParent, (([props]: useProfileThemeArgs, profileTheme: useProfileThemeRet) => {
+            const { user } = props;
+            if (
+                (user != null && user.id === previewUserId
+                || "pendingThemeColors" in props)
+                && showPreview
+            ) {
+                if (primaryColor !== null) {
+                    profileTheme.theme = getProfileTheme(primaryColor);
+                    profileTheme.primaryColor = primaryColor;
+                    profileTheme.secondaryColor = accentColor ?? primaryColor;
+                } else if (accentColor !== null) {
+                    profileTheme.theme = getProfileTheme(accentColor);
+                    profileTheme.primaryColor = accentColor;
+                    profileTheme.secondaryColor = accentColor;
+                }
             }
-        }
-        return profileTheme;
-    }
-    : ([user, _displayProfile, previewProps]: useProfileThemeColorsArgs, origRet: useProfileThemeColorsRet) => {
-        if (
-            (user != null && user.id === previewUserId
-            || previewProps)
-            && showPreview
-        ) {
-            if (primaryColor !== null)
-                return [primaryColor, accentColor ?? primaryColor];
-            if (accentColor !== null)
-                return [accentColor, accentColor];
-        }
-        return origRet;
-    }) as any
-);
+            return profileTheme;
+        }) as any);
+
+    funcParent = findByName("useProfileThemeColors", false);
+    if (funcParent)
+        return () => after("default", funcParent, (([user, _displayProfile, previewProps]: useProfileThemeColorsArgs, origRet: useProfileThemeColorsRet) => {
+            if (
+                (user != null && user.id === previewUserId
+                || previewProps)
+                && showPreview
+            ) {
+                if (primaryColor !== null)
+                    return [primaryColor, accentColor ?? primaryColor];
+                if (accentColor !== null)
+                    return [accentColor, accentColor];
+            }
+            return origRet;
+        }) as any);
+
+    return () => () => true;
+})();

commit 2cda823d2e8b2d0a876bea100d97cee1931d5991

Fixed race condition (introduced in a4597f1) with `UserProfileStore.getUserProfile` causing plugin to error and disable

eslint.config.js
@@ -94,7 +94,10 @@ export default tseslint.config(
                 varsIgnorePattern: "^_",
                 ignoreRestSiblings: true
             }],
-            "@typescript-eslint/no-use-before-define": "error",
+            "@typescript-eslint/no-use-before-define": ["error", {
+                functions: false,
+                classes: false
+            }],
             "@typescript-eslint/no-useless-empty-export": "error",
             "@typescript-eslint/prefer-find": "error",
             "@typescript-eslint/prefer-namespace-keyword": "off",

plugins/FakeProfileThemesAndEffects/src/index.ts
@@ -13,7 +13,9 @@ import { Settings } from "@ui/pages";
 /** Updates the profile theme and effect used by YouScreen and BottomTabBar. */
 function updateProfileThemeAndEffect() {
     const user = UserStore.getCurrentUser();
-    const user_profile = UserProfileStore.getUserProfile(user.id)!;
+    if (!user) return;
+    const user_profile = UserProfileStore.getUserProfile(user.id);
+    if (!user_profile) return;
     FluxDispatcher.dispatch({
         type: "USER_PROFILE_FETCH_SUCCESS",
         user,

plugins/FakeProfileThemesAndEffects/src/lib/stores/UserStore.ts
@@ -64,5 +64,5 @@ export interface User {
 
 export const UserStore: {
     getUser: (userId: string | null | undefined) => User | undefined;
-    getCurrentUser: () => User;
+    getCurrentUser: () => User | undefined;
 } = findByStoreName("UserStore");

plugins/FakeProfileThemesAndEffects/src/ui/actionSheets/showEffectPicker.tsx
@@ -24,7 +24,7 @@ export function showEffectPicker(onSelect: (effect: ProfileEffect["config"] | nu
                     onSelect(effect);
                     hideActionSheet(SHEET_KEY);
                 }}
-                user={UserStore.getCurrentUser()}
+                user={UserStore.getCurrentUser()!}
                 currentEffectId={currentEffectId}
             />
         ),

plugins/FakeProfileThemesAndEffects/src/ui/components/Builder.tsx
@@ -27,7 +27,7 @@ export function Builder({ guildId }: BuilderProps) {
         resolveSemanticColor(theme, semanticColors.HEADER_SECONDARY!),
         resolveSemanticColor(theme, semanticColors.BACKGROUND_ACCENT!)
     ], [theme]);
-    const avatarColors = useAvatarColors(UserStore.getCurrentUser().getAvatarURL(guildId, 80), fillerColor, false);
+    const avatarColors = useAvatarColors(UserStore.getCurrentUser()!.getAvatarURL(guildId, 80), fillerColor, false);
 
     return (
         <FormCardSection